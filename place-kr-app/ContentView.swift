8ce0b852050ee3a598e2c696eb82470b316e5221
enebin <46271447+enebin@users.noreply.github.com>
2022-01-18 22:23:35 +0900

    initial	 commit

---
+++ place-kr-app/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  place-kr-app
+//
+//  Created by 이영빈 on 2022/01/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Temporary")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
