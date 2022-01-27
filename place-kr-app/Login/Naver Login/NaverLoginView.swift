45770630978f3fddc43256257f3cd03f37c3e9df
enebin <lyb35548@gmail.com>
2022-01-27 11:53:18 +0900

    Add: Naver login View model

---
+++ place-kr-app/Login/Naver Login/NaverLoginView.swift
@@ -0,0 +1,39 @@
+import SwiftUI
+import CoreData
+
+struct NaverLoginButtonView: View {
+    @Environment(\.managedObjectContext) var viewContext: NSManagedObjectContext
+    @FetchRequest(entity: UserProfile.entity(), sortDescriptors: []) var userProfile: FetchedResults<UserProfile>
+    
+    @Binding var success: Bool
+    @State var showNaverLogin = false
+    @ObservedObject var viewModel = NaverLoginButtonViewModel()
+
+    var body: some View {
+        
+        Button(action: { showNaverLogin = true }) {
+            Text("Naver로 로그인")
+                .font(.system(size: 20))
+        }
+        .foregroundColor(.white)
+        
+        if showNaverLogin {
+            NaverLoginView
+        }
+    }
+}
+
+extension NaverLoginButtonView {
+    /// Login pop up
+    var NaverLoginView: some View {
+        NaverVCRepresentable { result in
+            self.success = viewModel.completionHandler(result)
+        }
+    }
+}
+
+struct NaverLoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        NaverLoginButtonView(success: .constant(false))
+    }
+}
--- place-kr-app/Login/Naver Login/NaverLoginViewModel.swift
+++ place-kr-app/Login/Naver Login/NaverLoginViewModel.swift
@@ -1,27 +1,11 @@
-import SwiftUI
-import CoreData
+//
+//  NaverLoginViewModel.swift
+//  place-kr-app
+//
+//  Created by 이영빈 on 2022/01/27.
+//
 
-struct NaverLoginButtonView: View {
-    @Environment(\.managedObjectContext) var viewContext: NSManagedObjectContext
-    @FetchRequest(entity: UserProfile.entity(), sortDescriptors: []) var userProfile: FetchedResults<UserProfile>
-    
-    @Binding var success: Bool
-    @State var showNaverLogin = false
-    @ObservedObject var viewModel = NaverLoginButtonViewModel()
-
-    var body: some View {
-        
-        Button(action: { showNaverLogin = true }) {
-            Text("Naver로 로그인")
-                .font(.system(size: 20))
-        }
-        .foregroundColor(.white)
-        
-        if showNaverLogin {
-            NaverLoginView
-        }
-    }
-}
+import Foundation
 
 class NaverLoginButtonViewModel: ObservableObject {
     init() {
@@ -71,6 +55,8 @@ class NaverLoginButtonViewModel: ObservableObject {
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         
+        // Peek into request body
+//        print(String(data: body, encoding: String.Encoding.utf8))
         let task = session.uploadTask(with: request, from: body) { data, response, error in
             if let error = error {
                 print("Error while uploading")
@@ -78,6 +64,7 @@ class NaverLoginButtonViewModel: ObservableObject {
                 return
             }
 
+            // Peek into response
             print(response as Any)
             if let response = response {
                 print(response)
@@ -98,87 +85,9 @@ class NaverLoginButtonViewModel: ObservableObject {
             print(jsonData.getDescription())
         }
         task.resume()
-            
-    }
-}
-
-
-extension NaverLoginButtonView {
-    private struct NaverLoginRequestResponse: Codable {
-        let token: String
-        
-        func getDescription() {
-            print("Token: \(self.token)")
-        }
     }
     
-    /// Returning token type and token
-    var NaverLoginView: some View {
-        NaverVCRepresentable { result in
-            switch result {
-            case .failure(let error):
-                print(error)
-            case .success(let userInfo):
-                print("Successfully get token. Email:\(userInfo.email), Token:\(userInfo.accessToken)")
-                
-                let url = URL(string: "https://dev.place.tk/api/v1/auth/naver")!
-                let encoder = JSONEncoder()
-                guard let body = try? encoder.encode(userInfo) else {
-                    print("Error while encoding")
-                    // TODO: Error handle
-                    return
-                }
-
-                self.sendPostRequest(to: url, body: body) { result in
-                    switch result {
-                    case .failure(let error):
-                        print(error)
-                    case .success(let result):
-                        print(result)
-                    }
-                }
-                
-            }
-            self.success = true
-        }
-    }
     
-    private func sendPostRequest(to url: URL, body: Data, then handler: @escaping (Result<Data, Error>) -> Void) {
-        let completionHandler = handler
-        let session = URLSession.shared
-        var request = URLRequest(url: url, cachePolicy: .reloadIgnoringCacheData)
-        request.httpMethod = "POST"
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-        let task = session.uploadTask(with: request, from: body) { data, response, error in
-            if let error = error {
-                print("Error while uploading")
-                completionHandler(.failure(error))
-                return
-            }
-
-            print(response as Any)
-            if let response = response {
-                print(response)
-            }
-
-            guard let data = data else {
-                completionHandler(.failure(URLError.self as! Error))
-                print("Error in data")
-                return
-            }
-
-            print(String(data: data, encoding: String.Encoding.utf8) as Any)
-            let decoder = JSONDecoder()
-            guard let jsonData = try? decoder.decode(NaverLoginRequestResponse.self, from: data) else {
-                print("Error in parsing")
-                return
-            }
-            print(jsonData.getDescription())
-        }
-        task.resume()
-            
-    }
     
 //    /// Returning Userdata
 //    var NaverLoginView: some View {
@@ -219,8 +128,3 @@ extension NaverLoginButtonView {
 //    }
 }
 
-struct NaverLoginView_Previews: PreviewProvider {
-    static var previews: some View {
-        NaverLoginButtonView(success: .constant(false))
-    }
-}
