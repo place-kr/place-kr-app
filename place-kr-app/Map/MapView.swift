7517cff3cc1b89707b5df74a66fdd42593578a26
enebin <lyb35548@gmail.com>
2022-02-08 12:09:03 +0900

    Add: Search field in map view

--- place-kr-app/Map/MapView.swift
+++ place-kr-app/Map/MapView.swift
@@ -8,56 +8,19 @@
 import SwiftUI
 
 struct MapView: View {
-    @State var text = ""
-    @State var coord = (0.0, 0.0)
+    @ObservedObject var place = SearchFieldViewModel()
     var body: some View {
         ZStack {
             VStack {
-                SearchField
+                SearchFieldView(viewModel: place)
                 Spacer()
             }
             .zIndex(1)
-            UIMapView(coord: $coord)
+            UIMapView(place: place)
                 .edgesIgnoringSafeArea(.vertical)
         }
     }
 }
-
-extension MapView {
-    struct TextFieldSearchButton: ViewModifier {
-        @Binding var text: String
-        @Binding var coord: (Double, Double)
-        func body(content: Content) -> some View {
-            HStack {
-                Button(
-                    action: {
-                        self.text = ""
-                        self.coord = (37.5666805, 126.9784147)
-                    },
-                    label: {
-                        Image(systemName: "magnifyingglass")
-                            .foregroundColor(.black)
-                    }
-                )
-                content
-                    .padding(.leading, 5)
-            }
-        }
-    }
-    
-    var SearchField: some View {
-        TextField("현위치: {자동 입력}", text: $text)
-            .modifier(TextFieldSearchButton(text: $text, coord: $coord))
-            .multilineTextAlignment(.leading)
-            .frame(width: 280, height: 50)
-            .padding(.horizontal)
-            .background(Color.white)
-            .cornerRadius(7)
-            .shadow(color: .black.opacity(0.2), radius: 10, x: 0, y: 2)
-            .padding(.vertical)
-    }
-}
-
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
         MapView()
---
+++ place-kr-app/Map/SearchField/SearchFieldView.swift
@@ -0,0 +1,59 @@
+//
+//  SearchFieldView.swift
+//  place-kr-app
+//
+//  Created by 이영빈 on 2022/02/07.
+//
+
+import SwiftUI
+
+struct SearchFieldView: View {
+    @State var text = ""
+    @ObservedObject var viewModel: SearchFieldViewModel
+    
+    var body: some View {
+        TextField("현위치: {자동 입력}", text: $text)
+            .modifier(TextFieldSearchButton(viewModel: viewModel, text: $text))
+            .multilineTextAlignment(.leading)
+            .frame(width: 280, height: 50)
+            .padding(.horizontal)
+            .background(Color.white)
+            .cornerRadius(7)
+            .shadow(color: .black.opacity(0.2), radius: 10, x: 0, y: 2)
+            .padding(.vertical)
+    }
+    
+    init(viewModel: SearchFieldViewModel) {
+        self.viewModel = viewModel
+    }
+}
+
+extension SearchFieldView {
+    /// Add button at search field
+    private struct TextFieldSearchButton: ViewModifier {
+        @ObservedObject var viewModel: SearchFieldViewModel
+        @Binding var text: String
+        
+        func body(content: Content) -> some View {
+            HStack {
+                Button(
+                    action: {
+                        viewModel.fetchWrittenPlaces(text)
+                    },
+                    label: {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.black)
+                    }
+                )
+                content
+                    .padding(.leading, 5)
+            }
+        }
+    }
+}
+
+struct SearchFieldView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchFieldView(viewModel: SearchFieldViewModel())
+    }
+}
