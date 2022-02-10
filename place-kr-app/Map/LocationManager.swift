02acfd6ff07f9fc6f8b699e9c3e1cb72c2886fba
enebin <lyb35548@gmail.com>
2022-02-10 11:16:57 +0900

    Add: Location manager

---
+++ place-kr-app/Map/LocationManager.swift
@@ -0,0 +1,69 @@
+//
+//  LocationManager.swift
+//  Weatherman
+//
+//  Created by 이영빈 on 2022/02/04.
+//
+
+import Foundation
+import CoreLocation
+import Combine
+
+// TODO: Auth handling
+
+class LocationManager: NSObject, ObservableObject {
+    private let locationManager = CLLocationManager()
+    static let shared = LocationManager()
+    
+    @Published var locationName: String?
+    @Published var currentCoord: CLLocationCoordinate2D?
+    @Published var locationStatus: CLAuthorizationStatus?
+
+    override init() {
+        super.init()
+        DispatchQueue.global(qos: .utility).async {
+            self.locationManager.delegate = self
+            self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            self.locationManager.requestWhenInUseAuthorization()
+            self.locationManager.requestLocation()
+
+//            self.locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func setCurrentLocation() {
+        locationManager.requestLocation()
+    }
+    
+    var statusString: String {
+        guard let status = locationStatus else {
+            return "unknown"
+        }
+        
+        switch status {
+        case .notDetermined: return "notDetermined"
+        case .authorizedWhenInUse: return "authorizedWhenInUse"
+        case .authorizedAlways: return "authorizedAlways"
+        case .restricted: return "restricted"
+        case .denied: return "denied"
+        default: return "unknown"
+        }
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        DispatchQueue.main.async {
+            self.locationStatus = status
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        self.coord = location.coordinate
+    }
+}
