dba6539b6aafd774c2e4daee6a71d049aae7b126
enebin <lyb35548@gmail.com>
2022-02-28 16:45:16 +0900

    Add: Place list on tap effect

--- place-kr-app/Tabs/MyPlaceTab/MyPlacerowViewModel.swift
+++ place-kr-app/Tabs/MyPlaceTab/MyPlacerowViewModel.swift
@@ -10,9 +10,22 @@ import SwiftUI
 class MyPlaceRowViewModel: ObservableObject {
     @Published var listName: String
     @Published var places: [PlaceInfo]
+    @Published var selectionStateDict = [String: Bool]()
+    
+    func changeSelectionState(_ placeID: String) {
+        let isSelected = self.selectionStateDict[placeID, default: false]
+        if isSelected {
+            self.selectionStateDict[placeID] = false
+        } else {
+            self.selectionStateDict[placeID] = true
+        }
+    }
     
     init(name: String) {
         self.listName = name
         self.places = dummyPlaceInfoArray
+        _ = self.places.map({ place in
+            self.selectionStateDict[place.id] = false
+        })
     }
 }
--- place-kr-app/Tabs/MyPlaceTab/PlaceListDetailView.swift
+++ place-kr-app/Tabs/MyPlaceTab/PlaceListDetailView.swift
@@ -13,6 +13,50 @@ struct PlaceListDetailView: View {
     
     var body: some View {
         VStack(alignment: .leading) {
+            if isEditable {
+                editableView
+            } else {
+                SimplePlaceCardView(viewModel.listName,
+                                    subscripts: "\(viewModel.places.count) places",
+                                    image: UIImage())
+                    .frame(height: 100)
+                    .padding(.horizontal, 17)
+                    .background(
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(.white)
+                            .shadow(color: .gray.opacity(0.15), radius: 20, y: 2)
+                    )
+                
+                Text("총 \(viewModel.places.count)개의 플레이스")
+                    .font(.basic.subtitle)
+                
+                ScrollView {
+                    VStack(spacing: 10) {
+                        ForEach(viewModel.places, id: \.id) { place in
+                            PlaceCardView(bgColor: .white, placeInfo: place)
+                                .padding(.vertical)
+                                .background(
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .fill(.white)
+                                        .shadow(color: .gray.opacity(0.15), radius: 20, y: 2)
+                                )
+                        }
+                    }
+                }
+            }
+        }
+        .navigationBarItems(trailing: Button(action: { isEditable.toggle() }, label: {
+            Text("Edit")
+                .foregroundColor(.black)
+        }))
+        .navigationBarTitle("\(viewModel.listName)", displayMode: .inline) // TODO: 원본과 다름
+        .padding(.horizontal, 15)
+    }
+}
+
+extension PlaceListDetailView {
+    var editableView: some View {
+        Group {
             SimplePlaceCardView(viewModel.listName,
                                 subscripts: "\(viewModel.places.count) places",
                                 image: UIImage())
@@ -24,38 +68,40 @@ struct PlaceListDetailView: View {
                         .shadow(color: .gray.opacity(0.15), radius: 20, y: 2)
                 )
             
-            if !isEditable {
-                Text("총 \(viewModel.places.count)개의 플레이스")
-                    .font(.basic.subtitle)
-            } else {
-                Button(action: {}) {
-                    HStack {
-                        Image(systemName: "checkbox")
-                        Text("전체선택")
-                    }
+            HStack {
+                Button(action: {  }) {
+                    Image(systemName: "squreshape")
+                    Text("전체선택")
                 }
+                .font(.basic.subtitle)
+                .foregroundColor(.black)
             }
             
             ScrollView {
                 VStack(spacing: 10) {
                     ForEach(viewModel.places, id: \.id) { place in
+                        let placeID = place.id
+                        
                         PlaceCardView(bgColor: .white, placeInfo: place)
                             .padding(.vertical)
+                            .overlay (
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.black.opacity(viewModel.selectionStateDict[placeID, default: false] ? 1 : 0))
+                            )
                             .background(
                                 RoundedRectangle(cornerRadius: 10)
                                     .fill(.white)
                                     .shadow(color: .gray.opacity(0.15), radius: 20, y: 2)
                             )
+                            .contentShape(Rectangle())
+                            .onTapGesture {
+                                viewModel.changeSelectionState(placeID)
+                            }
+                        
                     }
                 }
             }
         }
-        .navigationBarItems(trailing: Button(action: { isEditable.toggle() }, label: {
-            Text("Edit")
-                .foregroundColor(.black)
-        }))
-        .navigationBarTitle("\(viewModel.listName)", displayMode: .inline) // TODO: 원본과 다름
-        .padding(.horizontal, 15)
     }
 }
 
