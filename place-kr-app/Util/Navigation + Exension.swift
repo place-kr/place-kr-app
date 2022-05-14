577398403fdaaa318b89c58026e121344463efd0
enebin <lyb35548@gmail.com>
2022-05-14 14:35:56 +0900

    Add swipe back for navigation views

---
+++ place-kr-app/Util/Navigation + Exension.swift
@@ -0,0 +1,20 @@
+//
+//  Navigation + Exension.swift
+//  place-kr-app
+//
+//  Created by 이영빈 on 2022/05/14.
+//
+
+import SwiftUI
+
+extension UINavigationController: UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
+
